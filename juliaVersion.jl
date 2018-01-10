374477b8c2cea551c473a2e52997f30bdcb87294
Travis Deane Ashworth <travisashworth2007@gmail.com>
2018-01-10 12:47:07 -0600

    Pollish code
    In keeping this as much like the original example as possible I went ahead and seeded the rand function. On the other hand, I used randn (normalised) instead since that is something that's built in in julia. I also took code out of global scope since that is highly discouraged in julia due to the speed penalty imposed.

--- juliaVersion.jl
+++ juliaVersion.jl
@@ -1,49 +1,50 @@
-# This runs twice as fast on my machine. Of course the objective was not really to compare speed.
-# Hope this is usefull to someone ;)
+# julia example based on:               https://www.youtube.com/embed/h3l4qz76JhQ
+# This runs much faster on my machine than the python(with numpy) version. Of course the objective
+# was not really to compare speed. Execution time may also vary and since I only ran the test once 
+# this can hardly be called a fair comparison. On the other hand the julia code could still be 
+# optimised to run faster but I'm trying to stick with the example.
 # ===============================================================================================
-# julia>                               finished in 10.75 seconds
-#
-# python >>> (with numpy)              finished in 20.24 seconds
+# python(with numpy)                    13.909520864486694
+# julia                               ÷ 3.449227
+#                                    ______________________
+#                        julia runs:    4.032648725 times faster
 # ===============================================================================================
 
 function nonlin(x, deriv=false)
-    if deriv==true
-        return (x*(1-x))
-    end
+    deriv==true && return (x*(1-x))
     return 1/(1+exp(-x))
 end
 
+# Wraped in a function so it runs faster... keep code out of global scope.
+function train()
+    
+    x = [ 0 0 1 ; 0 1 1 ; 1 0 1 ; 1 1 1  ]  # input
+    y = [0; 1; 1; 0]                        # output
 
-# input
-x = [ 0 0 1 ; 0 1 1 ; 1 0 1 ; 1 1 1  ]
-# output
-y = [0; 1; 1; 0]
+    rng = MersenneTwister(1234);
+    syn0 = randn(rng, (3,4)) # 3x4 matrix of normalised weights.
+    syn1 = randn(rng, (4,1)) # 4x1 matrix of normalised weights.
 
-syn0 = 2*rand(3,4)-1 # 3x4 matrix of weights. ((2 inputs + 1 bias) x 4 nodes in the hidden layer)
-syn1 = 2*rand(4,1)-1 # 4x1 matrix of weights. (4 nodes x 1 output) - no bias term in the hidden layer.
+    for j in 1:60000
 
+        # Calculate forward through the network.
+            l0 = x
+            l1 = nonlin.(l0 * syn0)
+            l2 = nonlin.(l1 * syn1) 
 
-for j in 1:60000
+        # Back propagation of errors using the chain rule.
+            l2_error = y - l2            
+            (j % 10000) == 0  && println("Error: ", mean(abs.(l2_error)) ) # print error every 10000 steps.
 
-    # Calculate forward through the network.
-        l0 = x
-        l1 = nonlin.(l0 * syn0)
-        l2 = nonlin.(l1 * syn1) 
+            l2_delta = l2_error .* nonlin.(l2, true) 
+            l1_error = l2_delta .* syn1' 
+            l1_delta = l1_error .* nonlin.(l1,true) 
 
-    # Back propagation of errors using the chain rule.
-        l2_error = y - l2 
-        if(j % 10000) == 0   # Only print the error every 10000 steps.
-            println("Error: ", mean(abs.(l2_error)) ) 
-        end
+        # Update weights (no learning rate term)
+            syn1 += l1' * l2_delta 
+            syn0 += l0' * l1_delta
 
-        l2_delta = l2_error .* nonlin.(l2, true) 
-        l1_error = l2_delta .* transpose(syn1) 
-        l1_delta = l1_error .* nonlin.(l1,true) 
-
-    # Update weights (no learning rate term)
-        syn1 += transpose(l1) * l2_delta 
-        syn0 += transpose(l0) * l1_delta
-
-        println("Output after training")
-        println(l2)
+            println("Output after training")
+            println(l2)
+    end
 end
